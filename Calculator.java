3e08a351e648c68f38975e6019ebc7efbab9a206
Shivani Jadhav <shivani@gmail.com>
2026-01-29 16:43:08 +0530

    Initial calculator project

---
+++ Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Addition: " + add(10, 5));
+        System.out.println("Subtraction: " + sub(10, 5));
+    }
+}
